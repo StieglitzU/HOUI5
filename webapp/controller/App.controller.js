f7e6dbcf6da50bf1c713aae1e0daba6332a8bf85
ho05 clouddnagmbhgmail.onmicrosoft.com <ho05@clouddnagmbhgmail.onmicrosoft.com>
2022-05-17 12:26:37 +0000

    Fragment Exercise

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -10,6 +10,15 @@ sap.ui.define([
         return Controller.extend("at.cloudna.zhoui505.controller.App", {
             onInit: function () {
 
-            }
+            },
+
+             genderFormatter:function(sKey){
+                 let oView = this.getView();
+                 let oI18nModel = oView.getModel("i18n");
+                let oResourceBundle = oI18nModel.getResourceBundle();
+                let sText = oResourceBundle.getText(sKey);
+                return sText;
+             }   
+
         });
     });
