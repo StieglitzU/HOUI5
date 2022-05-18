4ac15cbfe9115ab12b906ca8c502833f9f87a1c3
ho05 clouddnagmbhgmail.onmicrosoft.com <ho05@clouddnagmbhgmail.onmicrosoft.com>
2022-05-18 07:57:17 +0000

    Change Fragment - Edit / Save

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,17 +1,47 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageBox"
+    "sap/m/MessageBox",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/core/Fragment"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller, MessageBox) {
+    function (Controller, MessageBox, JSONModel, Fragment) {
         "use strict";
 
         return Controller.extend("at.cloudna.zhoui505.controller.App", {
-            onInit: function () {
 
-            },
+            _fragmentList : {}, 
+
+            onInit: function () {
+                    let oEditModel = new JSONModel({
+                        editmode: false}); 
+
+            this.getView().setModel(oEditModel, "editModel");
+            this._showCustomerFragment("displaycustomer")
+        },
+
+
+            _showCustomerFragment: function(sFragmentName){
+                let oPage = this.getView().byId("page");
+            
+                oPage.removeAllContent();
+            
+                if(this._fragmentList[sFragmentName]){
+                    oPage.insertContent(this._fragmentList[sFragmentName]);
+                }else{
+                    Fragment.load({
+                        id: this.getView().createId(sFragmentName),
+                        name: "at.cloudna.zhoui505.view.fragment." + sFragmentName,
+                        controller: this
+                    }).then(function(oFragment){
+                        this._fragmentList[sFragmentName] = oFragment;
+                        oPage.insertContent(this._fragmentList[sFragmentName]);
+                    }.bind(this));
+                }
+            },            
+
 
              genderFormatter:function(sKey){
                  let oView = this.getView();
@@ -20,11 +50,29 @@ sap.ui.define([
                 let sText = oResourceBundle.getText(sKey);
                 return sText;
              },
-             onSavePressed: function(){
-                 let oModel = this.getView().getModel();
-                 let oData = oModel.getData();
+             onEditPressed: function(){
+                this._toggleEdit(true);
+            },
+            
+            _toggleEdit: function(bEditMode){
+                let oEditModel = this.getView().getModel("editModel");
+            
+                oEditModel.setProperty("/editmode", bEditMode);
+            
+                this._showCustomerFragment(bEditMode ? "changecustomer" : "displaycustomer");
+            },
+            
+            onSavePressed: function(){
+                let oModel = this.getView().getModel();
+                let oData = oModel.getData();
                 MessageBox.success(JSON.stringify(oData));
-              },
+            
+                this._toggleEdit(false);
+            },
+            
+            onCancelPressed: function(){
+                this._toggleEdit(false);
+            },
              
 
         });
